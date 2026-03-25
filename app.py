3a2e36a45e87df44bba42b4ea8414661b66a8db5
ber <a0968066786b@gmail.com>
2026-03-25 10:12:43 +0800

    基本網站雛形正式出來)

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/direction')
+def direction():
+    return render_template('direction.html')
+
+@app.route('/plan')
+def plan():
+    return render_template('plan.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
